286a85c57f422e59db89db4c6c0bfa47a2d118ad
Luis Alho <lmalho@gmail.com>
2024-12-27 13:14:25 +0000

    send correlation id

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -9,6 +10,17 @@ import (
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+type Header struct {
+	RequestApiKey     int16
+	RequestApiVersion int16
+	CorrelationId     int32
+}
+type Response struct {
+	MessageSize int32
+	Header      Header
+	Body        string
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,9 +33,17 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer c.Close()
+
+	r := make([]byte, 8)
+	binary.BigEndian.PutUint32(r[0:4], 0)
+	binary.BigEndian.PutUint32(r[4:8], 7)
+
+	c.Write(r)
 }
